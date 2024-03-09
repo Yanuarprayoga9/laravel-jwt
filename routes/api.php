d5470cbbb6bf22424e842122f044be9660dd76cd
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-03-09 15:10:27 +0700

    feat: api register route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * route "/register"
+ * @method "POST"
+ */
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
